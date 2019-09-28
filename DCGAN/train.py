12e56e8b3d0161ef7b8978aa47937d55ddaae41d
Jigar Pael <30416256+drone911@users.noreply.github.com>
2019-09-28 20:00:30 +0530

    asthetic tweaks

--- DCGAN/train.py
+++ DCGAN/train.py
@@ -54,9 +54,10 @@ def train(train_images, generator, discriminator, gan, num_classes=120, random_d
             if e % 10 == 0:
                 discriminator.save_weights("models\\disc_v1_epoch_{}.h5".format(e))
                 generator.save_weights("models\\gen_v1_epoch_{}.h5".format(e))
-        except:
+        except KeyboardInterrupt:
             iterator.close()
             print("Interrupted")
+            break
 if __name__=="__main__":    
     (train_images, train_labels), (test_images, test_labels)=mnist.load_data()
     random_dim=100
