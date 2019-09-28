0a24384857a758223a5cef4f2339ba23c9d90866
Jigar Pael <30416256+drone911@users.noreply.github.com>
2019-09-28 13:24:10 +0530

    Update train.py

--- DCGAN/train.py
+++ DCGAN/train.py
@@ -23,37 +23,40 @@ def train(train_images, generator, discriminator, gan, num_classes=120, random_d
         real_img_y[:]=0.9
         gan_y=np.ones((batch_size, 1))
         hist_disc, hist_gen=[], []
-        
-        for i in tqdm(range(num_batches)):
-            sampled_noise = generate_inputs(random_dim, batch_size)
+        iterator=tqdm(range(num_batches))
+        try:
+            for i in iterator:
+                sampled_noise = generate_inputs(random_dim, batch_size)
+                
+                real_img_x=train_images[np.random.randint(0,train_images.shape[0],size=batch_size)]
+                fake_img_x=generator.predict(sampled_noise)
+                
+                train_disc_x=np.concatenate((real_img_x, fake_img_x), axis=0)
+                train_disc_y=np.concatenate((real_img_y, fake_img_y), axis=0)
+                
+                discriminator.trainable=True
+                hist_disc.append(discriminator.train_on_batch(train_disc_x, train_disc_y))
+                
+                noise=generate_inputs(random_dim, batch_size)
+                
+                discriminator.trainable=False
+                hist_gen.append(gan.train_on_batch(noise, gan_y))
+                
+            hist_disc_avg.append(np.mean(hist_disc[0:num_batches]))
+            hist_gen_avg.append(np.mean(hist_gen[0:num_batches]))
+            print("-------------------------------------------------------")
+            print("discriminator loss at epoch {}:{}".format(e, hist_disc_avg[-1]))
+            print("generator loss at epoch {}:{}".format(e, hist_gen_avg[-1]))
+            print("-------------------------------------------------------")
             
-            real_img_x=train_images[np.random.randint(0,train_images.shape[0],size=batch_size)]
-            fake_img_x=generator.predict(sampled_noise)
-            
-            train_disc_x=np.concatenate((real_img_x, fake_img_x), axis=0)
-            train_disc_y=np.concatenate((real_img_y, fake_img_y), axis=0)
-            
-            discriminator.trainable=True
-            hist_disc.append(discriminator.train_on_batch(train_disc_x, train_disc_y))
-            
-            noise=generate_inputs(random_dim, batch_size)
-            
-            discriminator.trainable=False
-            hist_gen.append(gan.train_on_batch(noise, gan_y))
-            
-        hist_disc_avg.append(np.mean(hist_disc[0:num_batches]))
-        hist_gen_avg.append(np.mean(hist_gen[0:num_batches]))
-        print("-------------------------------------------------------")
-        print("discriminator loss at epoch {}:{}".format(e, hist_disc_avg[-1]))
-        print("generator loss at epoch {}:{}".format(e, hist_gen_avg[-1]))
-        print("-------------------------------------------------------")
-        
-        plot_generated_images(e, generator, random_dim=random_dim)
-        plot_loss(hist_disc, hist_gen)
-        if e % 10 == 0:
-            discriminator.save_weights("models\\disc_v1_epoch_{}.h5".format(e))
-            generator.save_weights("models\\gen_v1_epoch_{}.h5".format(e))
-        
+            plot_generated_images(e, generator, random_dim=random_dim)
+            plot_loss(hist_disc, hist_gen)
+            if e % 10 == 0:
+                discriminator.save_weights("models\\disc_v1_epoch_{}.h5".format(e))
+                generator.save_weights("models\\gen_v1_epoch_{}.h5".format(e))
+        except:
+            iterator.close()
+            print("Interrupted")
 if __name__=="__main__":    
     (train_images, train_labels), (test_images, test_labels)=mnist.load_data()
     random_dim=100
@@ -68,8 +71,4 @@ if __name__=="__main__":
     discriminator=get_disc_nn(lr=lr, beta_1=beta_1,verbose=False)
     
     gan=create_gan(discriminator, generator, random_dim=random_dim, lr=lr, beta_1=beta_1,verbose=False)
-    try:
-        train(train_images, generator, discriminator, gan, random_dim=random_dim, epochs=50, batch_size=128)    
-    except KeyboardInterrupt:
-        print("\nInterrupted")
-    
+    train(train_images, generator, discriminator, gan, random_dim=random_dim, epochs=50, batch_size=128)    
