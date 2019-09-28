20ecfab4fbc3bc7cc9f8c92cea4072200b0ca798
Jigar Pael <30416256+drone911@users.noreply.github.com>
2019-09-28 17:41:32 +0530

    Update CatGAN.py

--- catGAN/CatGAN.py
+++ catGAN/CatGAN.py
@@ -4,3 +4,26 @@ Created on Sat Sep 28 13:36:00 2019
 
 @author: drone911
 """
+class CatGAN():
+    
+    def __init_(self, img_size, num_channels, num_classes, latent_dim, G_LR, D_LR, BETA_1=0.5, BETA_2=0.999, verbose=False):
+        pass
+    def save_model(self, ):
+        pass
+    def load_model(self, ):
+        pass
+    def fit(self, train_X, train_Y):
+        pass
+    def predict(self, ):
+        pass
+    def _make_generator(self, ):
+        pass
+    def _make_discriminator(self, ):
+        pass
+    def _plot_loss(self, ):
+        pass
+    def _mutual_loss(self, ):
+        pass
+    def _sample_images(self, ):
+        pass
+    
